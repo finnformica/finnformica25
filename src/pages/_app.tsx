71d8c514e9f771a1d73463514fce5e96e9104bc2
finnformica <finnformica@gmail.com>
2024-12-27 18:26:25 +0000

    add metadata and website title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,36 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import { supplyMono } from "@/fonts";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${supplyMono.className} font-sans`}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        {/* favicon */}
+        <link rel="icon" href="/favicon.ico" />
+
+        {/* title */}
+        <title>Finn Formica</title>
+
+        {/* metadata */}
+        <meta charSet="UTF-8" />
+        <meta name="author" content="Finn Formica" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="keywords"
+          content="personal portfolio, nextjs, web development"
+        />
+        <meta
+          name="description"
+          content="Hi, I am a full-stack developer and designer. I love creating beautiful and functional websites."
+        />
+        <meta name="robots" content="all" />
+      </Head>
+      <main className={`${supplyMono.className} font-sans`}>
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
