f3c7bd2e84aa7121852a325ae773ec102c417779
finnformica <finnformica@gmail.com>
2025-01-12 10:45:06 +0000

    fix typescript and eslint errors

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -1,11 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-
 import { ArrowLeft, ArrowRight } from "lucide-react";
 import { useRef, useState } from "react";
 
-import { Image, ScrollControls, useScroll } from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
+import {
+  Image,
+  ImageProps,
+  ScrollControls,
+  useScroll,
+} from "@react-three/drei";
+import { Canvas, GroupProps, ThreeEvent, useFrame } from "@react-three/fiber";
 import { easing } from "maath";
 import * as THREE from "three";
 
@@ -15,26 +17,27 @@ import { SectionTitle } from "@/components/text";
 import { Button } from "@/components/ui/button";
 import "@/utils/3d-carousel";
 
-function Card({ url, ...props }) {
-  const ref = useRef(null);
+function Card({ url, ...props }: ImageProps & { url: string }) {
+  const ref = useRef<THREE.Mesh>(null);
   const [hovered, hover] = useState(false);
-  const pointerOver = (e) => (e.stopPropagation(), hover(true));
+  const pointerOver = (e: ThreeEvent<PointerEvent>) => (
+    e.stopPropagation(), hover(true)
+  );
   const pointerOut = () => hover(false);
   useFrame((state, delta) => {
-    easing.damp3(ref.current.scale, hovered ? 1.15 : 1, 0.1, delta);
+    easing.damp3(ref.current!.scale, hovered ? 1.15 : 1, 0.1, delta);
     easing.damp(
-      ref.current.material,
+      ref.current!.material,
       "radius",
       hovered ? 0.25 : 0.1,
       0.2,
       delta,
     );
-    easing.damp(ref.current.material, "zoom", hovered ? 1 : 1.5, 0.2, delta);
+    easing.damp(ref.current!.material, "zoom", hovered ? 1 : 1.5, 0.2, delta);
   });
   return (
     <Image
       ref={ref}
-      alt=""
       url={url}
       transparent
       side={THREE.DoubleSide}
@@ -62,10 +65,12 @@ function Carousel({ radius = 1.4, count = 8 }) {
   ));
 }
 
-function Rig(props) {
-  const ref = useRef(null);
+function Rig(props: GroupProps) {
+  const ref = useRef<THREE.Group>(null);
   const scroll = useScroll();
   useFrame((state, delta) => {
+    if (!ref.current || !state.events.update) return;
+
     ref.current.rotation.y = -scroll.offset * (Math.PI * 2); // Rotate contents
     state.events.update(); // Raycasts every frame rather than on pointer-move
     easing.damp3(
@@ -76,17 +81,16 @@ function Rig(props) {
     ); // Move camera
     state.camera.lookAt(0, 0, 0); // Look at center
   });
-  return <group ref={ref} {...props} />;
+  return <group {...props} ref={ref} />;
 }
 
 const Projects = () => {
   const numProjects = 10;
   const [activeSlide, setactiveSlide] = useState(0);
 
+  const prev = () => activeSlide > 0 && setactiveSlide(activeSlide - 1);
   const next = () =>
     activeSlide < numProjects - 1 && setactiveSlide(activeSlide + 1);
-
-  const prev = () => activeSlide > 0 && setactiveSlide(activeSlide - 1);
 
   const renderFooter = () => (
     <div className="container mx-auto flex flex-col items-center gap-8 p-4 md:h-20 md:flex-row md:gap-0">
@@ -115,7 +119,7 @@ const Projects = () => {
       </div>
       <div className="no-scrollbar relative flex grow flex-row items-center justify-center gap-8 overflow-x-scroll pt-8">
         <Canvas camera={{ position: [0, 0, 100], fov: 15 }}>
-          {/* <fog attach="fog" args={["#a79", 8.5, 12]} /> */}
+          <fog attach="fog" args={["#a79", 8.5, 12]} />
           <ScrollControls pages={4} infinite>
             <Rig rotation={[0, 0, 0.15]}>
               <Carousel />
--- src/utils/3d-carousel.ts
+++ src/utils/3d-carousel.ts
@@ -1,34 +1,31 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-
-import * as THREE from "three";
 import { extend } from "@react-three/fiber";
+import * as THREE from "three";
 
 // Paul West @prisoner849 https://discourse.threejs.org/u/prisoner849
 // https://discourse.threejs.org/t/simple-curved-plane/26647/10
 class BentPlaneGeometry extends THREE.PlaneGeometry {
-  constructor(radius, ...args) {
+  constructor(radius: number, ...args: any) {
     super(...args);
-    let p = this.parameters;
-    let hw = p.width * 0.5;
-    let a = new THREE.Vector2(-hw, 0);
-    let b = new THREE.Vector2(0, radius);
-    let c = new THREE.Vector2(hw, 0);
-    let ab = new THREE.Vector2().subVectors(a, b);
-    let bc = new THREE.Vector2().subVectors(b, c);
-    let ac = new THREE.Vector2().subVectors(a, c);
-    let r =
+    const p = this.parameters;
+    const hw = p.width * 0.5;
+    const a = new THREE.Vector2(-hw, 0);
+    const b = new THREE.Vector2(0, radius);
+    const c = new THREE.Vector2(hw, 0);
+    const ab = new THREE.Vector2().subVectors(a, b);
+    const bc = new THREE.Vector2().subVectors(b, c);
+    const ac = new THREE.Vector2().subVectors(a, c);
+    const r =
       (ab.length() * bc.length() * ac.length()) / (2 * Math.abs(ab.cross(ac)));
-    let center = new THREE.Vector2(0, radius - r);
-    let baseV = new THREE.Vector2().subVectors(a, center);
-    let baseAngle = baseV.angle() - Math.PI * 0.5;
-    let arc = baseAngle * 2;
-    let uv = this.attributes.uv;
-    let pos = this.attributes.position;
-    let mainV = new THREE.Vector2();
+    const center = new THREE.Vector2(0, radius - r);
+    const baseV = new THREE.Vector2().subVectors(a, center);
+    const baseAngle = baseV.angle() - Math.PI * 0.5;
+    const arc = baseAngle * 2;
+    const uv = this.attributes.uv;
+    const pos = this.attributes.position;
+    const mainV = new THREE.Vector2();
     for (let i = 0; i < uv.count; i++) {
-      let uvRatio = 1 - uv.getX(i);
-      let y = pos.getY(i);
+      const uvRatio = 1 - uv.getX(i);
+      const y = pos.getY(i);
       mainV.copy(c).rotateAround(center, arc * uvRatio);
       pos.setXYZ(i, mainV.x, y, -mainV.y);
     }
@@ -37,12 +34,13 @@ class BentPlaneGeometry extends THREE.PlaneGeometry {
 }
 
 class MeshSineMaterial extends THREE.MeshBasicMaterial {
+  time: any;
   constructor(parameters = {}) {
     super(parameters);
     this.setValues(parameters);
     this.time = { value: 0 };
   }
-  onBeforeCompile(shader) {
+  onBeforeCompile(shader: THREE.WebGLProgramParametersWithUniforms) {
     shader.uniforms.time = this.time;
     shader.vertexShader = `
       uniform float time;
