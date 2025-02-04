29948d93c87bbf15f81c407be8cdb4f1e53e95a6
finnformica <finnformica@gmail.com>
2025-02-04 14:06:19 +0000

    add preloader styles

--- src/components/sections/preloader.tsx
+++ src/components/sections/preloader.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import styles from "@/styles/preloader.module.css";
 
 const Preloader = () => {
   return (
-    <div className="absolute left-0 top-0 z-[999] h-screen w-screen bg-black text-white">
-      Preloader
+    <div className="absolute left-0 top-0 z-[999] flex h-screen w-screen items-center justify-center bg-black text-white">
+      <p className={styles.glitch} data-text="Loading...">
+        Loading...
+      </p>
     </div>
   );
 };
