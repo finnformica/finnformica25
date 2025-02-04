0ac29eaf3284ffb3634db6ebfc05b265c5ac9793
finnformica <finnformica@gmail.com>
2025-02-04 13:42:38 +0000

    create preloader screen

---
+++ src/components/sections/preloader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Preloader = () => {
+  return (
+    <div className="absolute left-0 top-0 z-[999] h-screen w-screen bg-black text-white">
+      Preloader
+    </div>
+  );
+};
+
+export default Preloader;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import Footer from "@/components/sections/footer";
 import Hero from "@/components/sections/hero";
 import Info from "@/components/sections/info";
 import Milestones from "@/components/sections/milestones";
+import Preloader from "@/components/sections/preloader";
 import Projects from "@/components/sections/projects";
 import Tech from "@/components/sections/tech";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
   const sections = [
     <Hero key="hero" />,
     <Info key="info" />,
@@ -17,8 +22,15 @@ export default function Home() {
     <Footer key="footer" />,
   ];
 
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
   return (
     <>
+      {isLoading && <Preloader />}
       <div className="h-screen w-screen snap-y snap-mandatory snap-always overflow-y-auto overflow-x-hidden">
         {sections.map((comp, i) => (
           <section
